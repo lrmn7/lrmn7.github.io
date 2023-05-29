553f69a05db435f928d4f4b2050647dd446f2282
L RMN <97253349+lrmn7@users.noreply.github.com>
2023-05-29 23:38:29 +0700

    Email fix ✉️

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -2,9 +2,6 @@ import { useSection } from "context/section";
 
 const SocialLinks: React.FC<{ page?: string }> = ({ page }) => {
   const { currentSection } = useSection();
-
-  const email = "lrmn.dev@gmail.com"; // Ganti dengan alamat email Anda
-
   return (
     <>
       {page === "index" ? (
@@ -28,10 +25,10 @@ const SocialLinks: React.FC<{ page?: string }> = ({ page }) => {
         <div className="hidden fixed left-10 bottom-0 md:flex flex-col w-6 h-56 items-center justify-between">
           <div className="-rotate-90 text-lg tracking-widest">
             <a
-              href={`mailto:${email}`}
+              href={`mailto:${process.env.NEXT_PUBLIC_EMAIL}`}
               className="link-outline hover:text-marrsgreen dark:hover:text-carrigreen"
             >
-              {email}
+              {process.env.NEXT_PUBLIC_EMAIL}
             </a>
           </div>
           <div className="w-40 h-1 bg-bgdark dark:bg-bglight rotate-90"></div>
